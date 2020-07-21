33f71ac08cf51407d613ef43afeffe51c9feb80f
Afzal95 <61465593+Afzal95@users.noreply.github.com>
2020-07-21 22:07:06 +0530

    Import export module practice

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import p, { doublePi, triplePi} from "./math.js";
-import * as P from "./math.js";
-
-console.log(P);
-
+import {add, multiply, subtract, divide} from "./calculator.js";
+//Import the add, multiply, subtract and divide functions
+//from the calculator.js file.
+//If successful, your website should look the same as the Final.png
 
 ReactDOM.render(
   <ul>
-    <li>{P.default}</li>
-    <li>{P.doublePi()}</li>
-    <li>{P.triplePi()}</li>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
   </ul>,
   document.getElementById("root")
 );
